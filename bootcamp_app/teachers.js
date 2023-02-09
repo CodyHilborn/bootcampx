3c81f8181e5cb2965a60a2892383791362cfb3ea
Cody Hilborn <codyhilborn11@gmail.com>
2023-02-09 03:08:44 +0000

    Sanitize code to protect against sql injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,16 +8,19 @@ const pool = new Pool({
 });
 
 const cohortMonth = process.argv[2];
+const values = [`%${cohortMonth}%`];
 
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${cohortMonth}%'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
-`)
+`
+
+pool.query(queryString, values)
   .then(res => res.rows.forEach(teacher => {
     console.log(`${cohortMonth}: ${teacher.teacher}`);
   }))
